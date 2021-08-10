9654ab30d5d2977dabe983b72e2b829ffed17620
Paul-Longo <imanazarite88@gmail.com>
2021-08-10 17:24:24 -0400

    added this.books with book title and author components. added this.state booknumber. added div tags for (previous and next)buttons and books.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,12 +5,32 @@ import TitleBar from './TitleBar/TitleBar';
 class App extends Component {
     constructor(props){
         super(props);
-        this.state = {};
+        this.books = [
+            {title: "Moby Dick", author: "Herman Melville"},
+            {title: "The Canterbury Tales", author: "Geoffrey Chaucer"},
+            {title: "Jesus Calling", author: "Sarah Young"}
+        ];
+        this.state = {
+            bookNumber: 0
+        };
     }
     render(){
         return (
             <div className="container-fluid">
                 <TitleBar />
+                <div className="row">
+                    <div className="col-md-4">
+                        {/*Button here to move to previous book viewed*/}
+                    </div>
+                    <div className="col-md-4">
+                        {/*Display book with cover here*/}
+                        <h1>{this.books[this.state.bookNumber].title}</h1>
+                        <h4>{this.books[this.state.bookNumber].author}</h4>
+                    </div>
+                    <div className="col-md-4">
+                        {/*Button here to move to next book viewed*/}
+                    </div>
+                </div>
             </div>
         )
     }
