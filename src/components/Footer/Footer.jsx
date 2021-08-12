9de2d79f6002bc292eda6e1dd90d23fad5e6c5af
Paul-Longo <imanazarite88@gmail.com>
2021-08-12 00:24:11 -0400

    Updated code within Footer.jsx and Footer.css

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,13 +3,9 @@ import './Footer.css';
 
 const Footer = (props) => {
     return (
-        <div className="row row-spacer">
-            <div className="col-md-12" style={{padding: 150}}>
-                <div className="footer-nav">
-                    <h4> &copy; 2021 dCC Library </h4>
-                </div>
-            </div>
-        </div>
+        <footer className='footer'>
+            <h4> &copy; 2021 dCC Library </h4>
+        </footer>
     );
 }
  
